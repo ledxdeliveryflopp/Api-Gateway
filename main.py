5142ad14ed0c51b15a847a51d4580a77c46d7142
LedxDeliveryFlopp <korstim18@gmail.com>
2024-03-27 21:54:16 +0700

    Add CORS settings.

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from src.gateway.router import gateway_api_router
 
 gateway_api = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:7000",
+    "http://localhost:6000",
+    "http://localhost:5000",
+]
+
+gateway_api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 gateway_api.include_router(gateway_api_router)
